5fd9f28a448bc103a9d7d3ee9d57973bc8e35b1c
ziqing-ang <32562808+ziqing-ang@users.noreply.github.com>
2020-02-01 14:24:21 +0800

    tw_cna_scrape_v2

--- scraping/webscraper.py
+++ scraping/webscraper.py
@@ -1,6 +1,6 @@
 # TO-DO:
-# Set keyword to detect ’武漢肺炎‘(wuhan pneumonia),‘國家衛健委’(NHC),‘新型冠狀病毒’(coronavirus) at main page
-# Debug Non-Type Error
+# [DONE] Set keyword to detect ’武漢肺炎‘(wuhan pneumonia),‘國家衛健委’(NHC),‘新型冠狀病毒’(coronavirus) at news article page
+# [DONE] Debug Non-Type Error
 # Connect to db
 
 
@@ -13,39 +13,47 @@ response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
 
-# url for news content specific page (for testing)
-#a = 'https://www.cna.com.tw/news/ahel/202001315007.aspx'
-#news_url = requests.get(a,timeout=5)
-#news_content = BeautifulSoup(news_url.content, "html.parser")
+# gather url of articles
+article_url = []
+for title in content.find_all('a', {"class": "menuUrl"}):
+    link = title.get('href')
+    article_url.append(link)
 
 
+# empty list to collect newsObject for each url
+newsObject_stack = []
 
-for title in content.find_all('a', {"class":"menuUrl"}):
-    # scrape from individual links
-    news_url = title.get('href')
-    news_response = requests.get(news_url, timeout=5)
+# filtering related news and extract information
+for news in article_url:
+    news_response = requests.get(news, timeout=5)
     news_content = BeautifulSoup(news_response.content, "html.parser")
 
-    # combining paragraphs of news article
-    article_lines = []
-    news_paragraphs = news_content.find_all('p')
-    for line in news_paragraphs:
-        text = line.string
-        article_lines.append(text)
+    keyword = ['武漢肺炎', '國家衛健委', '新型冠狀病毒', '口罩']
+    # keyword translation: wuhan pneumonia, NHC, coronavirus, face mask
 
-    # gather information, make dict for json output -> db
-    newsObject = {
-        'title': title.string,
-        'description': news_content.find('meta',{"name":"description"}).get('content'),
-        'content':"".join(article_lines),
-        'author':news_content.find('meta',{"itemprop":'author'}).get('content'),
-        'url': news_url,
-        'urlToImage':news_content.find('link',{"rel":'image_src'}).get('href'),
-        'publishedAt':news_content.find('meta',{"itemprop":'datePublished'}).get('content'), 
-        'siteName':"cna.com.tw",
-        'language':"ch_trad"
-       
+    article_keyword = news_content.find(
+        'meta', {'name': 'keywords'}).get('content')
+    # detect keyword, if present then extract information to dict
+    if any(words in article_keyword for words in keyword):
 
-    }
+        # combining paragraphs of news article
+        article_lines = []
+        news_paragraphs = news_content.find(
+            'div', {'class': 'paragraph'}).find_all('p')
+        for line in news_paragraphs:
+            article_lines.append(line.text)
 
-    print(newsObject)
+        newsObject = {
+            'title': news_content.find('article', {"class": "article"}).get('data-title'),
+            'description': news_content.find('meta', {"name": "description"}).get('content'),
+            'content': "".join(article_lines),
+            'author': news_content.find('meta', {"itemprop": 'author'}).get('content'),
+            'url': news,
+            'urlToImage': news_content.find('link', {"rel": 'image_src'}).get('href'),
+            'publishedAt': news_content.find('meta', {"itemprop": 'datePublished'}).get('content'),
+            'siteName': "cna.com.tw",
+            'language': "ch_trad"
+        }
+
+        newsObject_stack.append(newsObject)
+        # print(newsObject)

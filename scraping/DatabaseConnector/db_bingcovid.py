8df949a01dbb4f295500dd14ada09e22feeee8da
Samuel Lee <samueljklee@gmail.com>
2020-03-17 22:42:54 -0700

    change: db bing covid duplicated key

--- scraping/DatabaseConnector/db_bingcovid.py
+++ scraping/DatabaseConnector/db_bingcovid.py
@@ -40,7 +40,7 @@ def select():
 def insert(data_dict, target_table="test"):
     table_name = PROD_TABLE_NAME if target_table == "prod" else TEST_TABLE_NAME
     mycursor = mydb.cursor()
-    sql = "INSERT INTO {} (state, country, last_update, lat, lng, confirmed, deaths, recovered, posted_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE state = %s, country = %s, last_update = %s, lat = %s, lng = %s, confirmed = %s, deaths = %s, recovered = %s, posted_date = %s".format(
+    sql = "INSERT INTO {} (state, country, last_update, lat, lng, confirmed, deaths, recovered, posted_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE state = %s, country = %s, confirmed = %s, deaths = %s, recovered = %s".format(
         table_name
     )
     val = (
@@ -55,13 +55,9 @@ def insert(data_dict, target_table="test"):
         data_dict["posted_date"],
         data_dict["state"],
         data_dict["country"],
-        data_dict["last_update"],
-        data_dict["lat"],
-        data_dict["lng"],
         data_dict["confirmed"],
         data_dict["deaths"],
         data_dict["recovered"],
-        data_dict["posted_date"],
     )
     print("SQL query: ", sql, val)
     try:

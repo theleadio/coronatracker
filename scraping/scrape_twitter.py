0a4da27991918d379eb63dc31110051ce259364a
joshdors9 <joshdors@my.yorku.ca>
2021-03-16 15:05:01 -0400

    added aggregate to scrape_twitter.py

--- scraping/scrape_twitter.py
+++ scraping/scrape_twitter.py
@@ -3,13 +3,19 @@ import json
 import datetime
 
 
+class SearchQuery:
+    def __init__(self, query):
+        self.query = query
+
+
+search_queries = SearchQuery(
+    "WuhanVirus OR 2019nCoV OR Coronavirus OR WuhanCoronavirus OR coronaviruses OR coronavirusoutbreak OR coronavirus OR Covid-19 OR COVID-19 OR ChineseCoronavirus OR Coronaoutbreak")
 
 if __name__ == '__main__':
-    search_query = "WuhanVirus OR 2019nCoV OR Coronavirus OR WuhanCoronavirus OR coronaviruses OR coronavirusoutbreak OR coronavirus OR Covid-19 OR COVID-19 OR ChineseCoronavirus OR Coronaoutbreak"
     filename = "corona_twitter.json"
-    #filename = "{}.json".format(username)
+    # filename = "{}.json".format(username)
 
-    tweets = query_tweets(query=search_query, begindate=datetime.date(2019, 12, 30), enddate=datetime.date(2020, 1, 27))
+    tweets = query_tweets(query=search_queries, begindate=datetime.date(2019, 12, 30), enddate=datetime.date(2020, 1, 27))
     print("Found: {} tweets".format(len(tweets)))
 
     j = []

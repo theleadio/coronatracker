67dc52716c334fa8128cc74c92e1efe4aa73b0fe
StephenPiccone <46498775+StephenPiccone@users.noreply.github.com>
2021-03-16 14:37:31 -0400

    Reference Issues/PRs None
    What does this implementation fix/change?
    Similarly to last pull request, We are using the aggregate to group associated terms together, so they can be referenced by other functions using the root. The DateConv class does what the date_converter class did origionally however now uses aggregation This is mostly to clean up the class and makes it easier to read and debug.
    
    Any other comments?
    No

--- scraping/date_converter.py
+++ scraping/date_converter.py
@@ -1,12 +1,20 @@
 import re
 from datetime import datetime, timezone
 
-str = "Sat, 25 Jan 2020 01:52:22 +2000"
-all = re.findall(r"[\d]{1,2} [ADFJMNOS]\w* [\d]{4} \b(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [\+]{1}[0-9]{4}\b", str)
+Class DateConv:
+def _init_(self,strng,regAll,dateTimeObj):
+    self.string = strng
+    self.regAll = regAll
+    self.dateTimeObj = dateTimeObj
 
-datetime_str = all[0] # '09/19/18 13:55:26'
 
-datetime_object = datetime.strptime(datetime_str, '%d %b %Y %H:%M:%S %z')
+convDate1 = DateConv("Sat, 25 Jan 2020 01:52:22 +2000",
+                    re.findall(r"[\d]{1,2} [ADFJMNOS]\w* [\d]{4} \b(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [\+]{1}[0-9]{4}\b", str),
+                    datetime.strptime(datetime_str, '%d %b %Y %H:%M:%S %z'))
+
+#all = re.findall(r"[\d]{1,2} [ADFJMNOS]\w* [\d]{4} \b(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [\+]{1}[0-9]{4}\b", str)
+#datetime_str = all[0] # '09/19/18 13:55:26'
+#datetime_object = datetime.strptime(datetime_str, '%d %b %Y %H:%M:%S %z')
 
 print(type(datetime_object))
 print(datetime_object)  # printed in default format

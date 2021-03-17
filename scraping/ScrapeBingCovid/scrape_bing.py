4254d35624e5c339658e3db94df47b59b94f54b3
Bipeng Wang <43762676+wbipeng@users.noreply.github.com>
2021-03-17 21:09:37 +0800

    merge 2 files into 1

--- scraping/ScrapeBingCovid/scrape_bing.py
+++ scraping/ScrapeBingCovid/scrape_bing.py
@@ -27,7 +27,31 @@ from ScrapeRss.helpers import get_seed_page
 
 # BingCovid
 from ScrapeBingCovid.BingCovid import BingCovid
-
+from datetime import datetime
+from dateutil import parser
+class BingCovid:
+    def __init__(
+        self,
+        state=None,
+        country=None,
+        last_update=None,
+        lat=None,
+        lng=None,
+        confirmed=None,
+        deaths=None,
+        recovered=None,
+        posted_date=datetime.utcnow(),
+    ):
+        self.state = state
+        self.country = country
+        self.last_update = parser.parse(last_update) if last_update else last_update
+        self.lat = lat
+        self.lng = lng
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+        self.posted_date = posted_date
+        
 if __name__ == "__main__":
     db_bingcovid.connect()
     res = get_seed_page(API_URL).json()

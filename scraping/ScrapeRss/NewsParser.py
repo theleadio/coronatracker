abf7c4dfda5139a9bf187447ed26f454389dfc25
Bipeng Wang <43762676+wbipeng@users.noreply.github.com>
2021-03-17 23:26:02 +0800

    merge Newscontent to Newsparser

--- scraping/ScrapeRss/NewsParser.py
+++ scraping/ScrapeRss/NewsParser.py
@@ -204,3 +204,19 @@ class NewsParser:
     def add_news_to_extraction_queue(self):
         for news_object in self.news_list:
             EXTRACT_QUEUE.put(news_object)
+class NewsContent:
+    def __init__(
+        self,
+        news_url="",
+        title="",
+        author="",
+        description="",
+        published_at=None,
+        seed_source=None,
+    ):
+        self.news_url = news_url
+        self.title = title
+        self.author = author
+        self.description = description
+        self.published_at = published_at
+        self.seed_source = seed_source

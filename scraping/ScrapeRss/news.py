ad47340949f276a074d078fe7a4ae3eeb70f7bf7
Duaa Ali <duaaali@Duaas-iMac.local>
2021-03-17 20:58:19 -0400

    Encapsulated two News-related Files into an Aggregate Root Class

---
+++ scraping/ScrapeRss/news.py
@@ -0,0 +1,12 @@
+# Aggregate root class for NewsContent.py and NewsParser.py
+
+class news:
+    import NewsContent
+    import NewsParser
+
+    def __init__(self):
+        pass
+
+    def get_NewsContent(self):
+
+    def get_NewsParser(self):

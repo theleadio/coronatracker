09bf65de10a83a89f74d7d91eb7ba46a84ab089b
Duaa Ali <duaaali@Duaas-iMac.local>
2021-03-17 20:39:37 -0400

    Encapsulated Two Files into a New Aggregated Root Class

---
+++ scraping/ScrapeBingCovid/covid_bing.py
@@ -0,0 +1,15 @@
+# Aggregate root class for BingCovid.py and scrape_bing.py
+
+class covid_bing:
+    import BingCovid
+    import scrape_bing
+
+    def __init__(self):
+        pass
+
+    def get_BingCovid:
+        pass
+
+    def get_scrape_bing:
+        pass
+

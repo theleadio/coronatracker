d353cb6ff24f043639a50b1b70429f27bfe6d699
ssw626 <80845560+ssw626@users.noreply.github.com>
2021-03-17 21:47:10 -0400

    Add files via upload

--- scraping/DatabaseConnector/db_malaysia_patient_cases with states.py
+++ scraping/DatabaseConnector/db_malaysia_patient_cases with states.py
@@ -44,28 +44,23 @@ if (mysql.search("States",txt))
     def insert(data_dict, target_table="test"):
         table_name = PROD_TABLE_NAME if target_table == "prod" else TEST_TABLE_NAME
         mycursor = mydb.cursor()
-        sql = "INSERT INTO {} (caseId, status, statusDate, confirmedDate, nationality, age, gender, hospital, description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE caseId = %s, status = %s, statusDate = %s, confirmedDate = %s, nationality = %s, age = %s, gender = %s, hospital = %s, description = %s".format(
+        sql = "INSERT INTO {} (state, increment_count, total_count, hospital_count, recovered_count, death_count, last_updated) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE state = %s, increment_count = %s, total_count = %s, hospital_count = %s, recovered_count = %s, death_count = %s".format(
             table_name
         )
         val = (
-            data_dict["case"],
-            data_dict["status"],
-            data_dict["status_date"],
-            data_dict["confirmed_date"],
-            data_dict["nationality"],
-            data_dict["age"],
-            data_dict["gender"],
-            data_dict["hospital"],
-            data_dict["description"],
-            data_dict["case"],
-            data_dict["status"],
-            data_dict["status_date"],
-            data_dict["confirmed_date"],
-            data_dict["nationality"],
-            data_dict["age"],
-            data_dict["gender"],
-            data_dict["hospital"],
-            data_dict["description"],
+            data_dict["state"],
+            data_dict["increment_count"],
+            data_dict["total_count"],
+            data_dict["hospital_count"],
+            data_dict["recovered_count"],
+            data_dict["death_count"],
+            data_dict["last_updated"],
+            data_dict["state"],
+            data_dict["increment_count"],
+            data_dict["total_count"],
+            data_dict["hospital_count"],
+            data_dict["recovered_count"],
+            data_dict["death_count"],
         )
         print("SQL query: ", sql, val)
         try:
@@ -77,7 +72,7 @@ if (mysql.search("States",txt))
             print("Record not inserted")
 else
     def connect():
-    global mydb
+        global mydb
 
     # populate this from env file
     path_to_json = "./db.json"

6dc9cec08b91948d0f6fade76dbe443260c322a3
ramia bajwa <ramia_bajwa@hotmail.com>
2021-03-19 14:34:07 -0400

    Aggregates for the date conversion

--- scraping/date_converter.py
+++ scraping/date_converter.py
@@ -13,3 +13,24 @@ print(datetime_object)  # printed in default format
 print(datetime_object.astimezone(timezone.utc))
 
 
+import re
+from datetime import datetime, timezone
+
+#Aggregate root
+Class Timestamp:
+
+def __init__(self, date, time):
+
+self.date = date
+self.time = time
+
+str = "Sat, 25 Jan 2020 01:52:22 +2000"
+all = re.findall(r"[\d]{1,2} [ADFJMNOS]\w* [\d]{4} \b(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [\+]{1}[0-9]{4}\b", str)
+
+datetime_str = all[0] # '09/19/18 13:55:26'
+
+datetime_object = Timestamp.strptime(datetime_str, '%d %b %Y %H:%M:%S %z')
+
+print(type(datetime_object))
+print(datetime_object)  # printed in default format
+print(datetime_object.astimezone(timezone.utc))

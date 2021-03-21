db2520990f4abc5c9a8e8c5b561d3b8c674706db
lisal22 <lisal22@my.yorku.ca>
2021-03-20 21:21:09 -0400

    Aggregate for scrape_twitter.py

--- scraping/scrape_twitter.py
+++ scraping/scrape_twitter.py
@@ -2,6 +2,15 @@ from twitterscraper import query_tweets
 import json
 import datetime
 
+class Twitter: 
+    def __init__(self, username, tweet_id, hashtags, links, timestamp, text):
+        self.username = username
+        self.tweet_id = tweet_id
+        self.hashtags = hashtags
+        self.links = links
+        self.timestamp = timestamp
+        self.text = text
+
 if __name__ == '__main__':
     search_query = "WuhanVirus OR 2019nCoV OR Coronavirus OR WuhanCoronavirus OR coronaviruses OR coronavirusoutbreak OR coronavirus OR Covid-19 OR COVID-19 OR ChineseCoronavirus OR Coronaoutbreak"
     filename = "corona_twitter.json"

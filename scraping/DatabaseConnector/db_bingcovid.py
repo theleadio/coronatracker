59297e6723d52123f744d595959efebf4dea3ece
Peter Tim Soriano <chops10@my.yorku.ca>
2021-04-13 02:24:36 -0400

    Creational Pattern (Builder)

--- scraping/DatabaseConnector/db_bingcovid.py
+++ scraping/DatabaseConnector/db_bingcovid.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+from db_builder import Director
 
 # BING_COVID TABLE_SCHEMA
 # ['nid', 'state', 'country', 'last_update', 'lat', 'lng', 'confirmed', 'deaths', 'recovered', 'posted_date']
@@ -37,28 +38,17 @@ def select():
         print(x)
 
 
-def insert(data_dict, target_table="test"):
+def insert(target_table="test"):
     table_name = PROD_TABLE_NAME if target_table == "prod" else TEST_TABLE_NAME
     mycursor = mydb.cursor()
     sql = "INSERT INTO {} (state, country, last_update, lat, lng, confirmed, deaths, recovered, posted_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE state = %s, country = %s, confirmed = %s, deaths = %s, recovered = %s".format(
         table_name
     )
-    val = (
-        data_dict["state"],
-        data_dict["country"],
-        data_dict["last_update"],
-        data_dict["lat"],
-        data_dict["lng"],
-        data_dict["confirmed"],
-        data_dict["deaths"],
-        data_dict["recovered"],
-        data_dict["posted_date"],
-        data_dict["state"],
-        data_dict["country"],
-        data_dict["confirmed"],
-        data_dict["deaths"],
-        data_dict["recovered"],
-    )
+    
+    #passing the value as a dict instead of a tuple
+    #will output the same value once put into the method execute() below
+    val = Director.construct_bc()
+
     print("SQL query: ", sql, val)
     try:
         mycursor.execute(sql, val)

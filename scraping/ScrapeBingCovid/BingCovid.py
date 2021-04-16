e0fa38818086817f19074a31a1cb15f3ffc75700
tbakshi <angel7897890@gmail.com>
2021-04-15 22:55:10 -0400

    Composite Pattern has been applied by separating entities observed in BingCovid

--- scraping/ScrapeBingCovid/BingCovid.py
+++ scraping/ScrapeBingCovid/BingCovid.py
@@ -1,26 +1,70 @@
 from datetime import datetime
 from dateutil import parser
 
+class CaseInfo:
+    def __init__(
+        self,
+        confirmed=None,
+        deaths=None,
+        recovered=None,
+        stateInfo=None,
+    ):
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+        self.stateInfo = stateInfo
+
+class CountryInfo:
+    def __init__(
+        self,
+        country=None,
+        lat=None,
+        lng=None,
+    ):
+        self.country = country
+        self.lat = lat
+        self.lng = lng
+
+
+class StateInfo:
+    def __init__(
+        self,
+        state=None,
+        countryInfo=None,
+    ):
+        self.state = state
+        self.countryInfo = countryInfo
+
 
 class BingCovid:
     def __init__(
         self,
-        state=None,
-        country=None,
-        last_update=None,
-        lat=None,
-        lng=None,
         confirmed=None,
         deaths=None,
         recovered=None,
+        last_update=None,
+        lat=None,
+        lng=None,
+        state=None,
+        country=None,
         posted_date=datetime.utcnow(),
     ):
-        self.state = state
-        self.country = country
+        self.countryInfo = CountryInfo(country, lat, lng)
+        self.stateInfo = StateInfo(state, self.countryInfo)
+        self.caseInfo = CaseInfo(confirmed, deaths, recovered, self.stateInfo)
+
         self.last_update = parser.parse(last_update) if last_update else last_update
-        self.lat = lat
-        self.lng = lng
-        self.confirmed = confirmed
-        self.deaths = deaths
-        self.recovered = recovered
         self.posted_date = posted_date
+
+    def getDataAsDict(self):
+        rt = {}
+        rt["confirmed"] = self.caseInfo.confirmed
+        rt["deaths"] = self.caseInfo.deaths
+        rt["recovered"] = self.caseInfo.recovered
+        rt["last_update"] = self.last_update
+        rt["lat"] = self.countryInfo.lat
+        rt["lng"] = self.countryInfo.lng
+        rt["state"] = self.stateInfo.state
+        rt["country"] = self.countryInfo.country
+        rt["posted_date"] = self.posted_date
+        return rt
